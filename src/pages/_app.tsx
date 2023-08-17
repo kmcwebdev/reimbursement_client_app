02bd69c8078eb553a7a2c7e77e52f73fab546ac5
jyzrgandia <jayzur.gandia@kmc.solutions>
2023-08-17 12:06:26 +0800

    Added Auth loader

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import store from "~/app/store";
 import PageAnimation from "~/components/animation/PageAnimation";
 import Layout from "~/components/core/layout";
+import AuthLoader from "~/components/loaders/AuthLoader";
 import { env } from "~/env.mjs";
 import "~/styles/globals.css";
 
@@ -24,13 +25,12 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <RequiredAuthProvider
       authUrl={env.NEXT_PUBLIC_PROPELAUTH_URL}
-      displayWhileLoading={<div>Auth Loading....</div>}
+      displayWhileLoading={<AuthLoader />}
       displayIfLoggedOut={<RedirectToCustomLocation />}
     >
       <Provider store={store}>
         <Layout>
           <PageAnimation>
-
             <Component {...pageProps} />
           </PageAnimation>
         </Layout>
