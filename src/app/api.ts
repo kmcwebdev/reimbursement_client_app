df3664101875b7255e5ec84ee2a4f75bfd4b63f6
jyzrgandia <jayzur.gandia@kmc.solutions>
2023-08-17 11:00:30 +0800

    Changes

--- src/app/api.ts
+++ src/app/api.ts
@@ -7,9 +7,9 @@ import {
   type FetchBaseQueryError,
   type FetchBaseQueryMeta,
 } from "@reduxjs/toolkit/query/react";
-import { RootState } from "./store";
-import { propelauthRefreshToken } from "~/utils/propelauthRefreshToken";
 import { clearAccessToken, setAccessToken } from "~/features/user-slice";
+import { propelauthRefreshToken } from "~/utils/propelauthRefreshToken";
+import { type RootState } from "./store";
 
 const appApiBaseQuery = fetchBaseQuery({
   baseUrl: "",
