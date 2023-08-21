f0f27ef056b82551f34ec78ab53f8ed66457750f
Gelo <chrisgelosulit@gmail.com>
2023-08-21 10:10:22 +0800

    Refactor _app.tsx to use new AuthProvider and WithAuthInfo components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,10 @@
-import { RequiredAuthProvider, useRedirectFunctions } from "@propelauth/react";
+import {
+  AuthProvider,
+  type WithAuthInfoProps,
+  useRedirectFunctions,
+  withAuthInfo,
+  type WithAuthInfoArgs,
+} from "@propelauth/react";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import store from "~/app/store";
@@ -6,34 +12,44 @@ import Layout from "~/components/core/layout";
 import { UserAccessProvider } from "~/context/AccessContext";
 import "~/styles/globals.css";
 import { env } from "~/env.mjs";
+import { type PropsWithChildren } from "react";
 
-const RedirectToCustomLocation = () => {
-  const { redirectToLoginPage } = useRedirectFunctions();
+const RedirectToCustomLocation = withAuthInfo<
+  WithAuthInfoProps & WithAuthInfoArgs & PropsWithChildren
+>(
+  (props) => {
+    const { redirectToLoginPage } = useRedirectFunctions();
 
-  redirectToLoginPage({
-    postLoginRedirectUrl:
-      env.NEXT_PUBLIC_ENVIRONMENT === "development"
-        ? `${window.location.href}`
-        : `https://${window.location.hostname}`,
-  });
+    if (!props.isLoggedIn) {
+      redirectToLoginPage({
+        postLoginRedirectUrl:
+          env.NEXT_PUBLIC_ENVIRONMENT === "development"
+            ? `${window.location.href}`
+            : `https://${window.location.hostname}`,
+      });
 
-  return null;
-};
+      return null;
+    }
+
+    return <>{props.children}</>;
+  },
+  {
+    displayWhileLoading: <div>Auth Loading....</div>,
+  },
+);
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <RequiredAuthProvider
-      authUrl={env.NEXT_PUBLIC_PROPELAUTH_URL}
-      displayWhileLoading={<div>Auth Loading....</div>}
-      displayIfLoggedOut={<RedirectToCustomLocation />}
-    >
-      <Provider store={store}>
-        <UserAccessProvider>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </UserAccessProvider>
-      </Provider>
-    </RequiredAuthProvider>
+    <AuthProvider authUrl={env.NEXT_PUBLIC_PROPELAUTH_URL}>
+      <RedirectToCustomLocation>
+        <Provider store={store}>
+          <UserAccessProvider>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </UserAccessProvider>
+        </Provider>
+      </RedirectToCustomLocation>
+    </AuthProvider>
   );
 }
