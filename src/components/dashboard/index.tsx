9ba3390aa14f957691eab2383bb9551670c1d0be
jyzrgandia <jayzur.gandia@kmc.solutions>
2023-08-30 11:32:20 +0800

    Fixed import error

--- src/components/dashboard/index.tsx
+++ src/components/dashboard/index.tsx
@@ -3,7 +3,7 @@ import { useUserAccessContext } from "~/context/AccessContext";
 import FinanceDashboard from "./Finance";
 import HrbpDashboard from "./Hrbp";
 import ManagerDashboard from "./Manager";
-import EmployeeDashboard from "./Employee";
+import EmployeeDashboard from "./employee";
 
 const DashboardComponent: React.FC = () => {
   const { user } = useUserAccessContext();
