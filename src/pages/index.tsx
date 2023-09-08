b25ca948cfde81a23bad89bf8a9d0f838f03f081
Gelo <chrisgelosulit@gmail.com>
2023-09-08 16:33:39 +0800

    Remove unused code and dependencies for Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,3 @@
-import { getUserFromServerSideProps } from "@propelauth/nextjs/server/pages";
-import { type GetServerSideProps } from "next";
 import { Button } from "~/components/core/Button";
 
 export default function Home() {
@@ -13,22 +11,3 @@ export default function Home() {
     </section>
   );
 }
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const user = await getUserFromServerSideProps(context);
-
-  if (!user) {
-    return {
-      redirect: {
-        destination: "/api/auth/login",
-        permanent: false,
-      },
-    };
-  }
-
-  return {
-    props: {
-      userJson: JSON.stringify(user),
-    },
-  };
-};
