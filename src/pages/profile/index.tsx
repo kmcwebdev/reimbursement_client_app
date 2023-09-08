7c70f39335ed9df06de9c521d97ee21408f1d31a
Gelo <chrisgelosulit@gmail.com>
2023-09-08 16:28:40 +0800

    Import `getUserFromServerSideProps` and `GetServerSideProps` in index.tsx, profile/index.tsx, and reimbursements/index.tsx files.In each file, add a `getServerSideProps` function that retrieves the user from the server-side props.If the user does not exist, redirect to "/api/auth/login".Return the user as a prop in each file.Remove unnecessary console.log statement.Commit message: "Import getUserFromServerSideProps and add getServerSideProps"

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,3 +1,5 @@
+import { getUserFromServerSideProps } from "@propelauth/nextjs/server/pages";
+import { type GetServerSideProps } from "next";
 import Head from "next/head";
 import React, { Fragment } from "react";
 import { type IconType } from "react-icons-all-files";
@@ -24,3 +26,22 @@ const Profile: React.FC = () => {
 };
 
 export default Profile;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const user = await getUserFromServerSideProps(context);
+
+  if (!user) {
+    return {
+      redirect: {
+        destination: "/api/auth/login",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      userJson: JSON.stringify(user),
+    },
+  };
+};
--- src/pages/reimbursements/index.tsx
+++ src/pages/reimbursements/index.tsx
@@ -22,8 +22,6 @@ export default Reimbursements;
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const user = await getUserFromServerSideProps(context);
 
-  console.log(user?.getOrgs());
-
   if (!user) {
     return {
       redirect: {
