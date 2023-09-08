7a7c94a3202be0ebf3da1a5f950756ef4ca7eebf
Gelo <chrisgelosulit@gmail.com>
2023-09-08 15:53:40 +0800

    Refactor email templates to display "Click here to view" instead of the full URL

--- src/components/email-templates/HrbpApproval.tsx
+++ src/components/email-templates/HrbpApproval.tsx
@@ -29,10 +29,10 @@ export const HRBPApproval: React.FC<Readonly<HRBPApprovalTemplateProps>> = ({
     <Text>Expense Description: {expenseType}</Text>
     <Text>Expense Date: {expenseDate}</Text>
     <Text>Amount: {amount}</Text>
-    <Text className="flex">
+    <Text>
       Receipts Attached:{" "}
       <Link href={receiptsAttached} target="_blank">
-        {receiptsAttached}
+        Click here to view
       </Link>
     </Text>
     <Text className="text-justify">
--- src/components/email-templates/Rejected.tsx
+++ src/components/email-templates/Rejected.tsx
@@ -29,10 +29,10 @@ export const Rejected: React.FC<Readonly<RejectedTemplateProps>> = ({
     <Text>Expense Description: {expenseType}</Text>
     <Text>Expense Date: {expenseDate}</Text>
     <Text>Amount: {amount}</Text>
-    <Text className="flex">
+    <Text>
       Receipts Attached:{" "}
       <Link href={receiptsAttached} target="_blank">
-        {receiptsAttached}
+        Click here to view
       </Link>
     </Text>
     <Text className="text-justify">
