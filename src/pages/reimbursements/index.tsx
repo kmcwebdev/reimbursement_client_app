f8a3f8a144195649f7ce84a1f94e484642386793
jyzrgandia <jayzur.gandia@kmc.solutions>
2023-09-13 16:34:42 +0800

    Changed conditions

--- src/pages/reimbursements/index.tsx
+++ src/pages/reimbursements/index.tsx
@@ -2,7 +2,7 @@ import { getUserFromServerSideProps } from "@propelauth/nextjs/server/pages";
 import { type GetServerSideProps, type NextPage } from "next";
 import Head from "next/head";
 import PageAnimation from "~/components/animation/PageAnimation";
-import MyReimbursements from "~/components/shared/MyReimbursements";
+import ReimbursementList from "~/components/shared/ReimbursementList";
 
 interface DashboardSSRProps {
   userJson: string;
@@ -15,7 +15,7 @@ const Reimbursements: NextPage<DashboardSSRProps> = () => {
         <title>Reimbursements</title>
       </Head>
       <PageAnimation>
-        <MyReimbursements />
+        <ReimbursementList />
       </PageAnimation>
     </div>
   );
@@ -29,21 +29,6 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     return {
       redirect: {
         destination: "/api/auth/login",
-        permanent: false,
-      },
-    };
-  }
-
-  const userOrgs = user.getOrgs();
-  const assignedRole = userOrgs[0].assignedRole;
-
-  if (
-    assignedRole === "HRBP" ||
-    assignedRole === "External Reimbursement Approver Manager"
-  ) {
-    return {
-      redirect: {
-        destination: "/dashboard",
         permanent: false,
       },
     };
