3b683349c34fab82c90c238961a5174a3800acc1
jyzrgandia <jayzur.gandia@kmc.solutions>
2023-10-09 16:23:54 +0800

    Set timezone default to manila

--- src/utils/parse-timezone.ts
+++ src/utils/parse-timezone.ts
@@ -1,10 +1,12 @@
 import dayjs from "dayjs";
-import timezone from 'dayjs/plugin/timezone';
-import utc from 'dayjs/plugin/utc';
+import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc";
 dayjs.extend(timezone);
 dayjs.extend(utc);
 
-export const parseTimezone =(dateString: string) => {
-  const timezone = dayjs.tz.guess();
-  return dayjs.utc(dateString).tz(timezone);
-}
+export const parseTimezone = (dateString: string) => {
+  // dayjs.tz.setDefault('');
+
+  console.log("TIMEZONE", timezone);
+  return dayjs.utc(dateString).tz("Asia/Manila");
+};
