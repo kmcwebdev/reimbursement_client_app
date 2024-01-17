d93ca4b9cc83011ec9dd9c2dcd60dd962608e31c
Gelo <chrisgelosulit@gmail.com>
2024-01-17 12:44:26 +0800

    refactor(components): Ensure non-null pathname before processing in PageTitle

--- src/app/components/core/layout/Header/PageTitle.tsx
+++ src/app/components/core/layout/Header/PageTitle.tsx
@@ -5,32 +5,39 @@ import React from "react";
 const PageTitle: React.FC = () => {
   const pathname = usePathname();
 
-  if (pathname.includes("email-action")) {
+  // Check if pathname is not null before proceeding with includes checks
+  if (pathname) {
+    if (pathname.includes("email-action")) {
+      return (
+        <div className="relative h-6 w-[101px]">
+          <Image
+            src="https://cdn.kmc.solutions/project-statics/KMC-logo-updated-black%20(1).png"
+            alt="kmc-logo"
+            sizes="100%"
+            fill
+          />
+        </div>
+      );
+    }
+
+    if (pathname.includes("forbidden")) {
+      return <h3 className="font-bold uppercase text-navy">Forbidden</h3>;
+    }
+
+    if (pathname.includes("page-not-found")) {
+      return <h3 className="font-bold uppercase text-navy">Page not Found</h3>;
+    }
+
+    // Replace all slashes and hyphens in the pathname with spaces for display
     return (
-      <div className="relative h-6 w-[101px]">
-        <Image
-          src="https://cdn.kmc.solutions/project-statics/KMC-logo-updated-black%20(1).png"
-          alt="kmc-logo"
-          sizes="100%"
-          fill
-        />
-      </div>
+      <h3 className="font-bold uppercase text-navy">
+        {pathname.replaceAll("/", "").replaceAll("-", " ")}
+      </h3>
     );
   }
 
-  if (pathname.includes("forbidden")) {
-    return <h3 className="font-bold uppercase text-navy">Forbidden</h3>;
-  }
-
-  if (pathname.includes("page-not-found")) {
-    return <h3 className="font-bold uppercase text-navy">Page not Found</h3>;
-  }
-
-  return (
-    <h3 className="font-bold uppercase text-navy">
-      {pathname.replaceAll("/", "").replaceAll("-", " ")}
-    </h3>
-  );
+  // Return a default title if pathname is null
+  return <h3 className="font-bold uppercase text-navy">Home</h3>;
 };
 
 export default PageTitle;
