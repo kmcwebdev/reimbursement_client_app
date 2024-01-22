36cf352041223278038635707b96f0a44dbe924c
jyzrgandia <jayzur.gandia@kmc.solutions>
2024-01-22 14:32:34 +0800

    next auth config changes

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -23,11 +23,14 @@ declare module "next-auth" {
   }
 }
 
-interface JWT {
-  id_token?: string;
-  provider?: string;
-  accessToken?: string;
-  refreshToken?: string;
+declare module "next-auth/jwt" {
+  /** Returned by the `jwt` callback and `getToken`, when using JWT sessions */
+  interface JWT {
+    id_token?: string;
+    provider?: string;
+    accessToken?: string;
+    refreshToken?: string;
+  }
 }
 
 declare module "@tanstack/react-table" {
