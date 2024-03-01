739c57ab67cc8e79421d998c344427b916856327
jyzrgandia <jayzur.gandia@kmc.solutions>
2024-03-01 09:58:02 +0800

    removed redundant use server

--- src/app/auth/login/login.ts
+++ src/app/auth/login/login.ts
@@ -4,8 +4,6 @@ import { signIn } from "~/app/auth";
 import { type Credentials } from "~/schema/auth.schema";
 
 export const handleCredentialsLogin = async (credentials: Credentials) => {
-  "use server";
-
   await signIn("credentials", {
     ...credentials,
     redirectTo: "/",
@@ -13,7 +11,6 @@ export const handleCredentialsLogin = async (credentials: Credentials) => {
 };
 
 export const handleAzureAdLogin = async () => {
-  "use server";
   await signIn(
     "azure-ad",
     {
