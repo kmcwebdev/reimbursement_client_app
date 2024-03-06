85e4f9186e4f034fe6587cfde409d3df9be50f93
jyzrgandia <jayzur.gandia@kmc.solutions>
2024-03-06 20:50:34 +0800

    Refactored File Upload

--- src/app/components/shared/reimburse-form/steps/AddAttachments/AttachmentsList.tsx
+++ src/app/components/shared/reimburse-form/steps/AddAttachments/AttachmentsList.tsx
@@ -7,22 +7,11 @@ import CollapseHeightAnimation from "~/app/components/animation/CollapseHeight";
 import IndeterminateProgressBar from "~/app/components/loaders/IndeterminateProgressBar";
 import { useAppDispatch, useAppSelector } from "~/app/hook";
 import { setReimbursementFormValues } from "~/features/state/reimbursement-form-slice";
+import { type AttachedFile } from ".";
 
 interface AttachmentsListProps {
-  uploadedAttachments: {
-    name: string;
-    status: "uploading" | "uploaded";
-    type: string;
-  }[];
-  setUploadedAttachments: React.Dispatch<
-    React.SetStateAction<
-      {
-        name: string;
-        status: "uploading" | "uploaded";
-        type: string;
-      }[]
-    >
-  >;
+  uploadedAttachments: AttachedFile[];
+  setUploadedAttachments: React.Dispatch<React.SetStateAction<AttachedFile[]>>;
 }
 
 const AttachmentsList: React.FC<AttachmentsListProps> = ({
@@ -43,15 +32,15 @@ const AttachmentsList: React.FC<AttachmentsListProps> = ({
       {uploadedAttachments.length > 0 &&
         uploadedAttachments.map((attachment, i) => (
           <div
-            key={attachment.name}
+            key={attachment.file.name}
             className="flex w-full items-center gap-3 rounded-md border p-2 px-3"
           >
             <div className="grid h-7 w-7 place-items-center rounded-full border border-neutral-300">
-              {attachment.type.includes("image") ? (
+              {attachment.file.type.includes("image") ? (
                 <IoMdImage className="h-4 w-4 text-navy" />
               ) : (
                 <>
-                  {attachment.type.includes("pdf") ? (
+                  {attachment.file.type.includes("pdf") ? (
                     <FaFilePdf className="h-4 w-4 text-navy" />
                   ) : (
                     <FaFileCsv className="h-4 w-4 text-navy" />
@@ -62,10 +51,10 @@ const AttachmentsList: React.FC<AttachmentsListProps> = ({
 
             <span className="w-10/12 gap-2 truncate  text-sm text-neutral-900">
               {attachment.status === "uploaded" ? (
-                <p>{reimbursementFormValues.attachments[i].file_name}</p>
+                <p>{attachment.file.name}</p>
               ) : (
                 <div className="flex flex-col gap-2">
-                  <p>Getting file URL..</p>
+                  <p>{attachment.file.name}</p>
                   <IndeterminateProgressBar />
                 </div>
               )}
@@ -81,7 +70,7 @@ const AttachmentsList: React.FC<AttachmentsListProps> = ({
                 const reimbursementAttachments =
                   reimbursementFormValues.attachments;
                 const updated = reimbursementAttachments.filter(
-                  (a) => a.file_name !== attachment.name,
+                  (a) => a.file_name !== attachment.file.name,
                 );
 
                 dispatch(
