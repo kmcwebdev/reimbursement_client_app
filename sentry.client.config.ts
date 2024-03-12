0a59f27e35e010d225a7edc13e808202a375b28b
jyzrgandia <jayzur.gandia@kmc.solutions>
2024-03-12 09:13:57 +0800

    changed sentry config

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -1,11 +1,11 @@
 // This file configures the initialization of Sentry on the client.
 // The config you add here will be used whenever a users loads a page in their browser.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
-
 import * as Sentry from "@sentry/nextjs";
+import { env } from "process";
 
 Sentry.init({
-  dsn: process.env.SENTRY_DSN,
+  dsn: env.SENTRY_DSN,
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
