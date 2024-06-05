e4737d3882058b1fd952d0820aa44e1b6ddc15e1
jayzurgandia <jayzur.gandia@kmc.solutions>
2024-06-05 20:29:12 +0800

    Removed loader from index page

--- src/app/components/WelcomePage.tsx
+++ src/app/components/WelcomePage.tsx
@@ -1,22 +1,28 @@
 "use client";
 
 import { type NextPage } from "next";
-import { redirect } from "next/navigation";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 import { useAppSelector } from "../hook";
-import AuthLoader from "./loaders/AuthLoader";
 import InitialLoginForm from "./shared/initial-login";
 
 const WelcomePage: NextPage = () => {
+  const router = useRouter();
   const { user } = useAppSelector((state) => state.session);
 
-  if (user && user.profile && !user.profile.first_login) {
-    redirect("/dashboard");
-  }
+  useEffect(() => {
+    if (user) {
+      if (user.profile) {
+        if (!user.profile.first_login) {
+          router.push("/dashboard");
+        }
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [user]);
 
   return (
     <>
-      {!user?.profile?.first_login && <AuthLoader />}
-
       {user?.profile?.first_login && (
         <section className="grid h-screen w-screen place-items-center">
           <div className="flex w-3/12 flex-col items-center gap-4 rounded-md bg-white p-4 shadow-md">
